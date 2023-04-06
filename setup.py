d66394e2c8264ccc6caef6c3efd2c7462e0d5533
Yash Choudhary <4yashchoudhary@gmail.com>
2023-04-07 02:06:18 +0530

    Need python practice

--- setup.py
+++ setup.py
@@ -9,11 +9,12 @@ def get_requirements(file_path:str)->list[str]:
         requirements=[req.replace("/n", " ") for req in requirements]
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
+    return requirements
 
 setup(
 name='mlproject',
 version='0.0.1',
-author='Yash',
+author='Yash', 
 author_email='4yashchoudhary@gmail.com',
 packages=find_packages(),
 install_requires=get_requirements('requirements.txt')
